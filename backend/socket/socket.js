41efd1a5ead14782927ebe0152a15a9d5ff650c1
MukundAgarwal-code <agarwalmukund1176@gmail.com>
2025-08-05 10:41:04 +0530

    abcwnk

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -6,13 +6,10 @@ const app = express();
 
 const server = http.createServer(app);
 const io = new Server(server, {
-    cors:{
-        origin:[
-            'http://localhost:3000',
-            'https://chat-app-k2sv.onrender.com/'
-        ],
-        methods:['GET', 'POST'],
-    },
+    cors: {
+        origin: ["https://chat-app-k2sv.onrender.com", "http://localhost:3000"],
+        methods: ["GET", "POST"]
+    }
 });
 
 export const getReceiverSocketId = (receiverId) => {
